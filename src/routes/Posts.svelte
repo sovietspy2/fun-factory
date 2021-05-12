0afe16e9137248a5154e6d25c631531649ed62e3
drexleradam <drexler1333@gmail.com>
2021-05-12 17:39:33 +0200

    post beginning

--- src/routes/Posts.svelte
+++ src/routes/Posts.svelte
@@ -1,1 +1,21 @@
-<p>posts</p>
+<script>
+    let data;
+
+    const create = () => {
+        debugger;
+        data = fetch('http://localhost:8080/posts').then((response) => response.json()).then((result) => {
+            console.log('Success:', result);
+        })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    };
+
+</script>
+
+<div>
+    <button on:click={create}>Get</button>
+    <h1>Posts:</h1>
+
+    <p>asd</p>
+</div>
